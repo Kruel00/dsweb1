8049e5bc5a560b6171b536634062bae809016b68
Javier Castorena <kastor.kruel@gmail.com>
2021-11-12 16:34:10 -0800

    plus

--- js/site.js
+++ js/site.js
@@ -1,3 +1,15 @@
+
+   
+
+
+document.addEventListener("DOMContentLoaded", () =>{
+
+        carrusel(imagenes, numImage);
+
+} )
+
+
+console.log('antes de las var');
 var imagenes = [
     'img/Crypto/Bitcoin.png',
     'img/Crypto/Binance.png',
@@ -9,41 +21,33 @@ var imagenes = [
     'img/Crypto/Tether.png',
     'img/Crypto/TRON.png',
     'img/Crypto/XRP.png'],
-    cont = 0;
+    numImage = 0;
 
-function carrousell(contenedor) {
-    contenedor.addEventListener('click', e => {
-        let atras = contenedor.querySelector('.atras'),
-            adelante = contenedor.querySelector('.adelante'),
-            img = contenedor.querySelector('img'),
-            tgt = e.target;
+var textoCriptomonedas = [
+    "<b> <h2> ¿Qué es el bitcoin y cómo funciona? </h2> El bitcoin es una moneda virtual que ha adquirido un gran valor en los últimos años. Descubre cómo funciona, cómo comprarlo, cuánto cuesta y por qué su precio se mueve tanto en los mercados. El bitcoin tiene revolucionado al mundo financiero. No solo a las empresas y los inversionistas, sino incluso a los países. Algunos, como El Salvador, están a su favor y lo piensan usar como moneda de curso legal. Otros, como China, están en su contra y aplican regulaciones estrictas para limitar su uso. El hecho es que la palabra. Bitcoin está de moda y es imposible no haberla escuchado. ¿Dónde nació este activo? ¿Qué es? ¿Cómo funciona? ¿Por qué su precio ha subido tanto? Aquí te lo decimos.",
+    "<h2><b> Binance: la exchange más grande y que más cosas nos permite hacer con Bitcoin</h2> Binance lanzo su ICO el 3 de julio, poniendo en venta un total de 100 millones de tokens ERC20 de nombre BNB (Binance Coin). La venta de los tokens duro apenas 3 minutos y consiguió recaudar los 15 millones de dólares previstos. El 14 de julio, tan solo 11 días después de lanzar la ICO, se puso a disposición de los usuarios la exchange. Tuvo un registro masivo de usuarios tras el lanzamiento, generando algunos problemas de acceso debido al gran volumen de usuarios. Actualmente Binance está valorada en más de 2.000 millones de dólares, que es la que tiene mayor valoración de mercado.</b>",
+    "Cardano",
+    "Dogecoin",
+    "Etherium",
+    "Iota",
+    "Litecoin",
+    "Tether",
+    "Tron",
+    "XRP"]
 
+function carrusel(){
 
-        if (tgt == atras) {
-            if (cont > 0) {
-                img.src = imagenes[count - 1];
-                count--;
-            } else {
-                img.src = imagenes[imagenes.length - 1];
-                cont = imagenes.length - 1;
-            }
+    document.carrusel.src = imagenes[numImage];
+    document.getElementById('texto_top').innerHTML = textoCriptomonedas[numImage];
 
-
-        } else if (tgt == adelante) {
-            if (cont < imagenes.length - 1) {
-                img.src = imagenes[cont + 1];
-                cont++;
-            } else {
-                img.src = imagenes[0];
-                cont = 0;
-            }
-        }  
-
-
-    })
+    if(numImage <9 ){
+        numImage++;
+    }else{
+        numImage = 0;
+    }
 }
 
-document.addEventListener("DOMContentLoaded", () =>{
-    let contenedor = document.querySelector('.articulo-cuerpo');
-    carrousell(contenedor);
-} )
+setInterval(carrusel,5000);
+
+
+
